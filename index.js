c4311cd327bd7b4ccfe6ff3d80e7edfaa5dbff9b
Carl <carlbrainegebe@gmail.com>
2021-05-23 00:46:40 -0500

    commit tree ab7db8a369fd045b890fc206e17417eda98c8bdc

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
+// const record = [
+//     {year: "2018", result: "L"},
+//     {year: "2017", result: "W"},
+//     {year: "2016", result: "N/A"}
+//   ]
 
+
+// function superbowlWin(testArray) {
+// const time = testArray.find(yearOfDub).year;
+
+//     function yearOfDub(element) {
+//         return element.result === "W";
+//     }
+
+// //return time;
+// console.log(time);
+// }
+
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+  ]
+
+
+function superbowlWin(testArray) {
+
+    function yearOfDub(element, index) {
+        return element.result === "W";
+    }
+const time = testArray.find(yearOfDub);
+for(const key in time) {
+return (time.year); 
+}
+//return time;
+//return ans;
+}
